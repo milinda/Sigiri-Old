b4c3e074b91dc3d0cb075c61c9c007c1f6b66ab8
milinda.pathirage@gmail.com <milinda.pathirage@gmail.com@5f70fa56-4f2b-2fd5-bfa6-59d7521c06a9>
2011-11-27 04:32:53 +0000

    Class structure for Job Store implementation.
    git-svn-id: http://sigiri.googlecode.com/svn/scratch@68 5f70fa56-4f2b-2fd5-bfa6-59d7521c06a9

---
+++ modules/daemons/azure/SigiriAzureIntegration/SigiriAzureDaemon/Job.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SigiriAzureDaemon
+{
+    class Job
+    {
+        public string JobDescription { set; get; }
+        public string JobId { set; get; }
+        public string Status { set; get; }
+
+        public enum JobType
+        {
+            FileMovement,
+            ApplicationExecution
+        }
+
+        public JobType Type { set; get; }
+    }
+}
