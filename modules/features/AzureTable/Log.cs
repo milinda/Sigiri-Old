7fa75d3a7c2da9662ba9fbe83c942347c0100b16
kavitha0424@gmail.com <kavitha0424@gmail.com@5f70fa56-4f2b-2fd5-bfa6-59d7521c06a9>
2011-12-02 20:30:16 +0000

    git-svn-id: http://sigiri.googlecode.com/svn/scratch@75 5f70fa56-4f2b-2fd5-bfa6-59d7521c06a9

---
+++ modules/features/AzureTable/Log.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.WindowsAzure.StorageClient;
+using Microsoft.WindowsAzure;
+
+namespace AzureTable
+{
+    //Log entry
+    class Log: TableServiceEntity
+    {
+        public String logValue { get; set; }
+        public DateTime TimeUtc { get; set; }
+        public Guid Id { get; set; }
+
+        public Log()
+        {}
+        //each table is partitioned on partition keys, and this entry(row) has a unique partition key and a unique row key within the partition
+        public Log(Guid Id, DateTime timeUtc, string message)
+            : base(Log.GetParitionKey(Id), Log.GetRowKey(timeUtc))
+        {
+            this.TimeUtc = timeUtc;
+            this.Id = Id;
+            this.logValue = message;
+        }
+
+        public static string GetParitionKey(Guid id)
+        {
+             return id.ToString().Replace("-", "").ToLower();
+        }
+
+        public static string GetRowKey(DateTime time)
+        {
+            string str = time.ToString("yyyyMMdd_HH_mm_ss");
+            return str;
+           
+        }
+       
+        
+       
+       
+    }
+}
