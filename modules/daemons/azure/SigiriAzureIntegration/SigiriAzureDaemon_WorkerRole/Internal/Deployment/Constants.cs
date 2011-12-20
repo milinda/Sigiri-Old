8763c73bf9df97865570ff4be20bd885ac6de70a
milinda.pathirage@gmail.com <milinda.pathirage@gmail.com@5f70fa56-4f2b-2fd5-bfa6-59d7521c06a9>
2011-12-20 18:32:47 +0000

    Updated members of HostedService API. This will be used
    git-svn-id: http://sigiri.googlecode.com/svn/scratch@80 5f70fa56-4f2b-2fd5-bfa6-59d7521c06a9

---
+++ modules/daemons/azure/SigiriAzureIntegration/SigiriAzureDaemon_WorkerRole/Internal/Deployment/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SigiriAzureDaemon_WorkerRole.Internal.Deployment
+{
+    class Constants
+    {
+        public const string ServiceManagementNS = "http://schemas.microsoft.com/windowsazure";
+        public const string OperationTrackingIdHeader = "x-ms-request-id";
+        public const string VersionHeaderName = "x-ms-version";
+        public const string VersionHeaderContent = "2009-10-01";
+        public const string VersionHeaderContent20100401 = "2010-04-01";
+        public const string VersionHeaderContent20101028 = "2010-10-28";
+        public const string PrincipalHeader = "x-ms-principal-id";
+
+        public const string WebMethodPost = "POST";
+        public const string ContentTypeApplicationXML = "application/xml";
+    }
+}
--- modules/daemons/azure/SigiriAzureIntegration/SigiriAzureDaemon_WorkerRole/Internal/Deployment/HostedService.cs
+++ modules/daemons/azure/SigiriAzureIntegration/SigiriAzureDaemon_WorkerRole/Internal/Deployment/HostedService.cs
@@ -1,24 +1,190 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.StorageClient;
 
 namespace SigiriAzureDaemon_WorkerRole.Internal.Deployment
 {
-    class HostedService
+    internal class HostedService
     {
+        private readonly string _subscriptionId;
         private readonly string _name;
         private readonly string _label;
         private readonly X509Certificate2 _mgtCert;
         private readonly CloudBlobClient _blobClient;
+        private const string Location = "Anywhere US";
 
-        public HostedService(string name, string label, X509Certificate2 mgtCert)
+        public enum DeploymentStatus
         {
+            Running,
+            Suspended,
+            RunningTransitioning,
+            SuspendedTransitioning,
+            Starting,
+            Suspending,
+            Deploying,
+            Deleting
+        }
+
+        public HostedService(string subscriptionId, string name, string label, X509Certificate2 mgtCert)
+        {
+            _subscriptionId = subscriptionId;
             _name = name;
             _label = label;
             _mgtCert = mgtCert;
+            _blobClient =
+                CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("DataConnectionString")).
+                    CreateCloudBlobClient();
+        }
+
+        public void Create()
+        {
+            var requestUri = "https://management.core.windows.net/" + _subscriptionId + "/services/hostedservices";
+            var webRequest = CreateWebRequest(requestUri, Encoding.UTF8.GetBytes(GetCreateHostedServiceInputXML()));
+
+            Trace.TraceInformation("Creating Hosted Service: " + _name);
+
+            try
+            {
+                var state = new RequestState()
+                                {
+                                    Request = webRequest
+                                };
+                webRequest.BeginGetResponse(RespCallback, state);
+            }catch(Exception e)
+            {
+                Trace.TraceError("Error occurred during hosted service creation:\n" + e.Message);
+            }
+        }
+
+        public void Delete()
+        {
+        }
+
+        public void CreateDeployment()
+        {
+        }
+
+        public DeploymentStatus GetDeploymentStatus()
+        {
+            return DeploymentStatus.Running;
+        }
+
+        public void SuspendDeployment()
+        {
+        }
+
+        public void ResumeDeployment()
+        {
+        }
+
+        public void DeleteDeployment()
+        {
+        }
+
+        private string GetCreateHostedServiceInputXML()
+        {
+            var requestXML = new StringBuilder("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            requestXML.Append("<CreateHostedService xmlns=\"http://schemas.microsoft.com/windowsazure\">");
+            requestXML.AppendFormat("<ServiceName>{0}</ServiceName>", _name);
+            requestXML.AppendFormat("<Label>{0}</Label>", EncodeToBase64String(_label));
+            requestXML.AppendFormat("<Location>{0}</Location>", Location);
+            requestXML.Append("</CreateHostedService>");
+
+            return requestXML.ToString();
+        }
+
+        private string GetCreateDeploymentInputXML(string deploymentName, string workerRolePackageBlobUrl,
+                                                   string workerRoleConfigBlobUrl)
+        {
+            var requestXML = new StringBuilder("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            requestXML.Append("<CreateDeployment xmlns=\"http://schemas.microsoft.com/windowsazure\">");
+            requestXML.AppendFormat("<Name>{0}</Name>", deploymentName);
+            requestXML.AppendFormat("<PackageUrl>{0}</PackageUrl>", workerRolePackageBlobUrl);
+            requestXML.AppendFormat("<Label>{0}</Label>", EncodeToBase64String("testdeploymentlabel"));
+            requestXML.AppendFormat("<Configuration>{0}</Configuration>",
+                                    EncodeToBase64String(GetWorkerRoleConfigurationFileAsString()));
+            requestXML.Append("<StartDeployment>true</StartDeployment>");
+            requestXML.Append("</CreateDeployment>");
+            return requestXML.ToString();
+        }
+
+        private static string GetWorkerRoleConfigurationFileAsString()
+        {
+            // TODO: Implement liquid template base configuration file generation
+            return "";
+        }
+
+
+        private HttpWebRequest CreateWebRequest(string uri, byte[] formData)
+        {
+            var webRequest =
+                (HttpWebRequest) WebRequest.Create(
+                    new Uri(uri));
+
+            webRequest.Method = Constants.WebMethodPost;
+            webRequest.ClientCertificates.Add(_mgtCert);
+            webRequest.ContentType = Constants.ContentTypeApplicationXML;
+            webRequest.ContentLength = formData.Length;
+            webRequest.Headers.Add(Constants.VersionHeaderName, Constants.VersionHeaderContent20101028);
+
+            using (var post = webRequest.GetRequestStream())
+            {
+                post.Write(formData, 0, formData.Length);
+            }
+
+            return webRequest;
+        }
+
+        private static void RespCallback(IAsyncResult result)
+        {
+            var state = (RequestState) result.AsyncState; // Grab the custom state object
+            var request = (WebRequest) state.Request;
+
+            var response =
+                (HttpWebResponse) request.EndGetResponse(result); // Get the Response
+
+            var statusCode = response.StatusCode.ToString();
+
+            // A value that uniquely identifies a request made against the Management service. 
+            // For an asynchronous operation, 
+            // you can call get operation status with the value of the header to determine whether 
+            // the operation is complete, has failed, or is still in progress.
+            var reqId = response.GetResponseHeader("x-ms-request-id");
+
+            Trace.TraceInformation("Creation Return Value: " + statusCode);
+            Trace.TraceInformation("RequestId: " + reqId);
+        }
+
+        public static string EncodeToBase64String(string original)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(original));
+        }
+    }
+
+    public class RequestState
+    {
+        private const int BufferSize = 1024;
+        public StringBuilder RequestData;
+        public byte[] BufferRead;
+        public WebRequest Request;
+        public Stream ResponseStream;
+
+        public Decoder StreamDecode = Encoding.UTF8.GetDecoder(); // Create Decoder for appropriate enconding type.
+
+        public RequestState()
+        {
+            BufferRead = new byte[BufferSize];
+            RequestData = new StringBuilder(String.Empty);
+            Request = null;
+            ResponseStream = null;
         }
     }
 }
