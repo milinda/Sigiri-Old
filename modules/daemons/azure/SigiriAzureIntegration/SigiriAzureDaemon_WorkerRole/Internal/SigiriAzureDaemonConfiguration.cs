8e3af54ed8df22c2615c36abb1f0c80a117c63bb
milinda.pathirage@gmail.com <milinda.pathirage@gmail.com@5f70fa56-4f2b-2fd5-bfa6-59d7521c06a9>
2011-12-22 16:09:58 +0000

    git-svn-id: http://sigiri.googlecode.com/svn/scratch@89 5f70fa56-4f2b-2fd5-bfa6-59d7521c06a9

---
+++ modules/daemons/azure/SigiriAzureIntegration/SigiriAzureDaemon_WorkerRole/Internal/SigiriAzureDaemonConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SigiriAzureDaemon_WorkerRole.Internal
+{
+    class SigiriAzureDaemonConfiguration
+    {
+        public string AzureSubscriptionId { set; get; }
+        public X509Certificate2 AzureManagementCertificate { set; get; }
+        public string WorkerRoleConfigurationTemplate { set; get; }
+        public string WorkerRolePakcageBlobUrl { set; get; }
+        public string DataConnectionString { set; get; }
+    }
+}
