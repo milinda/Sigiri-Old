2b43c4b3c5ac68f83a3d8835ca2ca615041eca7a
milinda.pathirage@gmail.com <milinda.pathirage@gmail.com@5f70fa56-4f2b-2fd5-bfa6-59d7521c06a9>
2011-12-26 18:12:03 +0000

    Output message from competed job.
    git-svn-id: http://sigiri.googlecode.com/svn/scratch@94 5f70fa56-4f2b-2fd5-bfa6-59d7521c06a9

---
+++ modules/daemons/azure/SigiriAzureIntegration/SigiriAzureDaemon_WorkerRole/Internal/SigiriAzureOutMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace SigiriAzureDaemon_WorkerRole.Internal
+{
+    class SigiriAzureOutMessage
+    {
+        public string ApplicationId { set; get; }
+        public string JobId { set; get; }
+        public string Status { set; get; }
+
+        public static SigiriAzureOutMessage CreateSigiriAzureOutMessageFromXML(string outMessage)
+        {
+            var outMessageDoc = new XmlDocument();
+            outMessageDoc.LoadXml(outMessage);
+
+            return new SigiriAzureOutMessage()
+                       {
+                           ApplicationId = outMessageDoc.GetElementsByTagName("AppId")[0].InnerText,
+                           JobId = outMessageDoc.GetElementsByTagName("JobId")[0].InnerText,
+                           Status = outMessageDoc.GetElementsByTagName("Status")[0].InnerText
+                       };
+        }
+    }
+}
